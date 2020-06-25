058a0f1a7cfd4290c714f9701a9504dd8c4f5b81
PicoRadia <53488041+PicoRadia@users.noreply.github.com>
2020-06-25 23:07:09 +0100

    template one liner

---
+++ template.py
@@ -0,0 +1,21 @@
+import json,scrapy;
+[(scrapy.Request(
+url=''+str(page),
+method = 'GET',
+headers={
+		'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36' ,
+},),
+
+[
+open('books.json','a').write(json.dumps(
+{
+	'' : ''.join(r.css('').get()),
+	'' : ''.join(r.css('').get()),
+	'' : ''.join(r.css('').get()),
+	'' : ''.join(r.css('').get()),
+
+}
+,indent=2,ensure_ascii=False)+'\n')
+for r in response.css('')])
+
+for page in range(1,50)]
